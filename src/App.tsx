2d074bf47a5e6666f0c13cfe6cfeadf19fa97c89
Carlos Beraldo <carlosberaldo06@gmail.com>
2021-06-24 20:22:30 -0300

    Quarta aula - NLW

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { AuthContextProvider } from './context/AuthContext';
+import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
 
 
 function App() {
@@ -12,8 +14,13 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/rooms/news" component={NewRoom}></Route>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/rooms/new" exact component={NewRoom}></Route>
+          <Route path="/rooms/:id" component={Room}></Route>
+
+          <Route path="/admin/rooms/:id" component={AdminRoom}></Route>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter >
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,19 +7,44 @@ import { Button } from '../components/Button'
 import { useHistory } from 'react-router-dom'
 
 import { useAuth } from '../hooks/useAuth'
+import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 
 
 
 export function Home() {
     const history = useHistory();
     const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
 
-    function handleCreatRoom() {
+    async function handleCreatRoom() {
         if (!user) {
-            signInWithGoogle();
+            await signInWithGoogle();
         }
 
-        history.push('/rooms/news');
+        history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if (roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if (!roomRef.exists()) {
+            alert('Room does not exist');
+            return;
+        }
+
+        if (roomRef.val().endedAt) {
+            alert('Rooms already closed.');
+            return;
+        }
+
+        history.push(`rooms/${roomCode}`);
     }
 
     return (
@@ -39,10 +64,12 @@ export function Home() {
                     <div className="separator">
                         ou entre em uma sala
                     </div>
-                    <form>
+                    <form onSubmit={handleJoinRoom}>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
                         />
                         <Button type="submit">
                             Entrar na sala
